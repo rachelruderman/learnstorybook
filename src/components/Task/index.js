e91258a09cd811d84f5390aa2b594c0ef725a101
Rachel Ruderman <raquel.rudermano@gmail.com>
2020-07-01 23:31:16 -0700

    Add task layout

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,9 +1,49 @@
 import React from 'react';
 
-export const Task = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
-  return (
-    <div className="list-item">
-      <input type="text" value={title} readOnly={true} />
-    </div>
-  );
+export const Task = ({ task, onArchiveTask, onPinTask }) => {
+
+    const { id, state, title } = task;
+
+    const renderCheckbox = () => {
+        return (
+            <label className="checkbox">
+                <input
+                    type="checkbox"
+                    defaultChecked={state === 'TASK_ARCHIVED'}
+                    disabled={true}
+                    name="checked"
+                />
+                <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
+            </label>
+        )
+    }
+    const renderTitle = () => {
+        return (
+            <div className="title">
+                <input type="text" value={title} readOnly={true} placeholder="Input title" />
+            </div>
+        )
+    }
+
+    const renderPin = () => {
+        const isArchived = (state === 'TASK_ARCHIVED');
+        return (
+            <div className="actions" onClick={event => event.stopPropagation()}>
+                {!isArchived && (
+                    // eslint-disable-next-line jsx-a11y/anchor-is-valid
+                    <a onClick={() => onPinTask(id)}>
+                        <span className={`icon-star`} />
+                    </a>
+                )}
+            </div>
+        )
+    }
+
+    return (
+        <div className={`list-item ${state}`}>
+            {renderCheckbox()}
+            {renderTitle()}
+            {renderPin()}
+        </div>
+    );
 }
