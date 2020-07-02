95e0f37e7fcad43c99e97f1ffaaac9fae5ed39b6
Rachel Ruderman <raquel.rudermano@gmail.com>
2020-07-02 00:01:55 -0700

    Add PropTypes to TaskList

---
+++ src/components/TaskList/stories/propTypes.js
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types';
+import { Task } from '../../Task';
+
+export const propTypes = {
+    loading: PropTypes.bool,
+    tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
+    onPinTask: PropTypes.func.isRequired,
+    onArchiveTask: PropTypes.func.isRequired,
+};
