193940d2155849582240036c751c1f175a195818
Rachel Ruderman <raquel.rudermano@gmail.com>
2020-07-01 23:36:58 -0700

    Add propTypes

---
+++ src/components/Task/propTypes.js
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+
+export const propTypes = {
+    task: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        state: PropTypes.string.isRequired,
+    }),
+    onArchiveTask: PropTypes.func,
+    onPinTask: PropTypes.func,
+};
--- src/components/Task/stories.js
+++ src/components/Task/stories.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Task } from './index';
 import { taskData } from './_util/taskData';
 import { actionsData } from './_util/actionsData';
+import { propTypes } from './propTypes';
 
 export default {
     component: Task,
@@ -13,3 +14,5 @@ export const Default = () => <Task task={{ ...taskData }} {...actionsData} />;
 export const Pinned = () => <Task task={{ ...taskData, state: 'TASK_PINNED' }} {...actionsData} />;
 
 export const Archived = () => <Task task={{ ...taskData, state: 'TASK_ARCHIVED' }} {...actionsData} />
+
+Task.propTypes = propTypes;
