484507a79ece4667372388976f985dfb997d36bb
Rachel Ruderman <raquel.rudermano@gmail.com>
2020-07-02 11:22:06 -0700

    Clean up redux.js

--- src/lib/redux.js
+++ src/lib/redux.js
@@ -2,35 +2,27 @@
 // A true app would be more complex and separated into different files.
 import { createStore } from 'redux';
 
-// The actions are the "names" of the changes that can happen to the store
-export const actions = {
-    ARCHIVE_TASK: 'ARCHIVE_TASK',
-    PIN_TASK: 'PIN_TASK',
-};
+const ARCHIVE_TASK = 'TASK_ARCHIVED';
+const PIN_TASK = 'TASK_PINNED';
 
 // The action creators bundle actions with the data required to execute them
-export const archiveTask = id => ({ type: actions.ARCHIVE_TASK, id });
-export const pinTask = id => ({ type: actions.PIN_TASK, id });
-
-// All our reducers simply change the state of a single task.
-const taskStateReducer = (taskState) => {
-    return (state, action) => {
-        return {
-            ...state,
-            tasks: state.tasks.map(task =>
-                task.id === action.id ? { ...task, state: taskState } : task
-            ),
-        };
-    };
-}
+export const archiveTask = id => ({ type: ARCHIVE_TASK, id });
+export const pinTask = id => ({ type: PIN_TASK, id });
 
 // The reducer describes how the contents of the store change for each action
 export const reducer = (state, action) => {
     switch (action.type) {
-        case actions.ARCHIVE_TASK:
-            return taskStateReducer('TASK_ARCHIVED')(state, action);
-        case actions.PIN_TASK:
-            return taskStateReducer('TASK_PINNED')(state, action);
+        case ARCHIVE_TASK:
+        case PIN_TASK:
+            return {
+                ...state,
+                tasks: state.tasks.map(task => {
+                    const isMatch = (task.id === action.id);
+                    return (isMatch)
+                        ? { ...task, state: action.type }
+                        : task;
+                })
+            };
         default:
             return state;
     }
