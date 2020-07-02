b7a244f35e48b1580c51096f8a2997880232f2a5
Rachel Ruderman <raquel.rudermano@gmail.com>
2020-07-01 23:54:40 -0700

    Add comment on decorators

--- src/components/TaskList/stories/index.js
+++ src/components/TaskList/stories/index.js
@@ -4,6 +4,10 @@ import { actionsData } from '../../Task/stories/data/actionsData';
 import { withPinnedTasksData } from './data/withPinnedTasksData';
 import { defaultTasksData } from './data/defaultTasksData';
 
+/*
+    Decorators are a way to provide arbitrary wrappers to stories. In this case we’re using a decorator `key` on the default export to add some `padding` around the rendered component. They can also be used to wrap stories in “providers” –i.e. library components that set React context.
+*/
+
 export default {
     component: TaskList,
     title: 'TaskList',
